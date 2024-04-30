59bc2fc6d38fc820fc74c93e7ede5099d4f4bfc7
hakeem <hakeem@codeap.com>
2024-05-01 01:25:31 +0530

    hyderabad page setting

--- src/component/prices-hyderabad.jsx
+++ src/component/prices-hyderabad.jsx
@@ -1,10 +1,11 @@
 import React, { useState } from 'react';
 import { Box, Tabs, Tab } from '@mui/material';
+import { FaClock } from 'react-icons/fa'; // Import FaClock icon from react-icons/fa
 
 function PricesHyderabad() {
   const [value, setValue] = useState(0);
 
-  const handleChange = ( newValue) => {
+  const handleChange = (event, newValue) => {
     setValue(newValue);
   };
 
@@ -17,35 +18,30 @@ function PricesHyderabad() {
               <h2>About Prices</h2>
               <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
                 <Tabs value={value} onChange={handleChange} aria-label="basic tabs example">
-                  <Tab label="Item One" />
-                  <Tab label="Item Two" />
-                  <Tab label="Item Three" />
+                  <Tab label="Information" />
+                  <Tab label="Tour plan" />
+                  <Tab label="Location" />
                 </Tabs>
+                <div className="col-12 mt-3 mb-3">
+                  {value === 0 && (
+                    <div>
+                      <div className="row">
+                        <div className="col-12">
+                          <h2>15000 / per person</h2>
+                          <div className="d-flex align-items-center mt-3 mb-3 ">
+                            <FaClock size={18} /> {/* Use FaClock icon */}
+                            <h4 className='ms-3'> 8 Nights / 9 Days</h4>
+                            <FaClock className='ms-3' size={18} /> {/* Use FaClock icon */}
+                            <h4 className='ms-3'> 8 Nights / 9 Days</h4>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  )}
+                  {value === 1 && <div>Item Two Content</div>}
+                  {value === 2 && <div>Item Three Content</div>}
+                </div>
               </Box>
-              <div role="tabpanel" hidden={value !== 0}>
-                {value === 0 && (
-                  <div>
-                    <h3>Item One Content</h3>
-                    <p>This is the content for Item One.</p>
-                  </div>
-                )}
-              </div>
-              <div role="tabpanel" hidden={value !== 1}>
-                {value === 1 && (
-                  <div>
-                    <h3>Item Two Content</h3>
-                    <p>This is the content for Item Two.</p>
-                  </div>
-                )}
-              </div>
-              <div role="tabpanel" hidden={value !== 2}>
-                {value === 2 && (
-                  <div>
-                    <h3>Item Three Content</h3>
-                    <p>This is the content for Item Three.</p>
-                  </div>
-                )}
-              </div>
             </div>
           </div>
         </div>
